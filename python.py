c0e0a1e8cd9e1f49d8cd7e83ba8505aecf1a643d
Vijilee <vijileegeorgekurian2028@cs.sjcetpalai.ac.in>
2024-10-04 16:10:08 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,30 @@
+"""x = "sjcet"
+x = " pALAI"
+print (x)"""
+
+"""print("hello world ")"""
+"""x = str("vdgfyf")
+y =int(9)
+z = float(49.8)
+print(x,y,z)
+print(type(x))
+print(type(y))"""
+
+"""x, y, z = "vijilee", "george","kurian"
+print(x,y,z)"""
+"""x = int(90)
+y = int(80)
+z = x+y
+print(z)"""
+"""x = int(90)
+y = int(70)
+z = int (30)
+sum = x+y+z
+avg =sum/3
+print(avg)"""
+
+"""username = input("enter usrename" )
+print("the username is " +username"""
+
+"""a ="sjcet"
+print(a[0])"""
